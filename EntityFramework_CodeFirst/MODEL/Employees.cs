b49eac1998ae28db887b7bc74904b74fcfc0365d
marti <marti@PORTATIL-MARTI>
2024-02-19 17:01:44 +0100

    Caca

--- EntityFramework_CodeFirst/MODEL/Employees.cs
+++ EntityFramework_CodeFirst/MODEL/Employees.cs
@@ -8,5 +8,13 @@ namespace EntityFramework_CodeFirst.MODEL
 {
     class Employees
     {
+        public int EmployeeNumber { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Extension { get; set; }
+        public string Email { get; set; }
+        public string OfficeCode { get; set; }
+        public int ReportsTo { get; set; }
+        public string JobTitle { get; set; }
     }
 }
