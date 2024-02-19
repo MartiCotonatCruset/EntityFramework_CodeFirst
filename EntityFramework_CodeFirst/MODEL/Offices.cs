f736a10f16f811f12a731d487702c7dc7cd9b4b8
Adria <a2208022@institutmontilivi.cat>
2024-02-19 17:12:14 +0100

    ClassesDefinition

--- EntityFramework_CodeFirst/MODEL/Offices.cs
+++ EntityFramework_CodeFirst/MODEL/Offices.cs
@@ -8,5 +8,13 @@ namespace EntityFramework_CodeFirst.MODEL
 {
     class Offices
     {
+        public string OfficeCode { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string Territory { get; set; }
     }
 }
