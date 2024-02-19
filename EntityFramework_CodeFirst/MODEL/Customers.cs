b6dbca230b844d321f5bdc6865b271df26d06876
marti <marti@PORTATIL-MARTI>
2024-02-19 16:54:52 +0100

    Product lines, products, order detalis, orders and customers classes structured

--- EntityFramework_CodeFirst/MODEL/Customers.cs
+++ EntityFramework_CodeFirst/MODEL/Customers.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,18 @@ namespace EntityFramework_CodeFirst.MODEL
 {
     class Customer
     {
+        public int CustomerNumber { get; set; }
+        public string CustomerName { get; set; }
+        public string ContactLastName { get; set; }
+        public string ContactFirstName { get; set; }
+        public string Phone { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public int SalesRepEmployeeNumber { get; set; }
+        public double CreditLimit { get; set; }
     }
 }
