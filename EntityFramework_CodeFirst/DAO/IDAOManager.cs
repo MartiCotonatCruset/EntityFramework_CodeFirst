692473ab4253ccd7370d9aa233b0747fb3e67db8
Adria <a2208022@institutmontilivi.cat>
2024-02-26 15:42:02 +0100

    DAOPatternBegin

---
+++ EntityFramework_CodeFirst/DAO/IDAOManager.cs
@@ -0,0 +1,14 @@
+﻿using EntityFramework_CodeFirst.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework_CodeFirst.DAO
+{
+    public interface IDAOManager
+    {
+        
+    }
+}
